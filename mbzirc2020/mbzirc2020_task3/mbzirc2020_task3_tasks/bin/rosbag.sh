8d24fce7764ee1d091262e4317d99214f57aaec2
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2024-02-27 02:37:20 +0900

    [WIP] remove mbizrc project

--- mbzirc2020/mbzirc2020_task3/mbzirc2020_task3_tasks/bin/rosbag.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rosrun hydrus rosbag_control_data.sh /realsense1/odom/throttle /realsense1/odom /realsense2/odom/throttle /realsense2/odom /lepton_camera/color/image /lepton_camera/color/camera_info /lepton_camera/temperature/camera_info /lepton_camera/temperature/image /lepton_camera/threshold/camera_info /lepton_camera/threshold/image /target_object/pos /task3_start /estimator/sensor_plugin/vo2/data /estimator/sensor_plugin/vo1/data /estimator/sensor_plugin/imu/data
